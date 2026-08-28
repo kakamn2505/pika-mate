cb7124918a11df8575efec3fca1f6f17c200d988
Pietro Phelipe Romao Da Silva <00001107726396sp@aluno.educacao.sp.gov.br>
2026-08-28 19:26:00 -0300

    o mersin mecheu no html e js, para fazer o bagui da fase 3

--- cadastro-alunos/script.js
+++ cadastro-alunos/script.js
@@ -3,7 +3,9 @@ console.log("Sistema iniciado!");
 let nome = "Kayke";
 let idade = 18;
 let curso = "Informática";
-
+document.getElementById("idade").textContent = idade;
+document.getElementById("curso").textContent = curso;
+document.getElementById("nome").textContent = nome;
 console.log(nome);
 console.log(idade);
 console.log(curso);
